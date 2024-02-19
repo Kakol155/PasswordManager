83b09b16debbd78f39ab1ccb5a3ab8d2393cd2f8
Kakol155 <kamil.budzowski@davb.pl>
2024-02-19 20:26:18 +0100

    Update

---
+++ script.js
@@ -0,0 +1,36 @@
+function submitForm() {
+    var login = document.getElementById("login").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (!login || !email || !password) {
+        console.error('Complete all form fields.');
+        return;
+    }
+
+    var data = {
+        login: login,
+        email: email,
+        password: password
+    };
+
+    fetch('http://localhost:3000/save-data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network or server error');
+            }
+            return response.json();
+        })
+        .then(result => {
+            console.log('Data saved to the database:', result);
+        })
+        .catch(error => {
+            console.error('Error saving data:', error.message);
+        });
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test_db'
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+        throw err;
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
+
+const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS UserData (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        login VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    )
+`;
+
+connection.query(createTableQuery, err => {
+    if (err) {
+        console.error('Error creating table:', err);
+    } else {
+        console.log('UserData table created or already exists');
+    }
+});
+
+app.post('/save-data', (req, res) => {
+    const data = req.body;
+
+    const insertQuery = 'INSERT INTO UserData (login, email, password) VALUES (?, ?, ?)';
+    connection.query(insertQuery, [data.login, data.email, data.password], (err, result) => {
+        if (err) {
+            console.error('Error saving data:', err);
+            res.status(500).json({ error: 'Error saving data' });
+        } else {
+            console.log('Data saved to the database:', data);
+            res.json({ message: 'Data saved successfully' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`The server runs on the port ${port}`);
+});
